b6425b07a8d2658e6d4bc8e26dce30ef3909388d
Noah Seslar Britton <noahseslar@gmail.com>
2022-03-07 23:51:07 -0500

    api login + create acc using class website

--- models/user.js
+++ models/user.js
@@ -37,7 +37,7 @@ const userSchema = new Schema({
     }
   }],
 })
-// arbitrary change
+
 userSchema.methods.generateAuthToken = async function () {
   const user = this
 
--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const cors = require('cors');
 require('../db/mongoose')
 
 const userRouter = require('../routers/users')
 
 const app = express()
-app.use(express.json())
 
+app.use(express.json())
 app.use(userRouter)
+app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 const port = process.env.PORT
 
